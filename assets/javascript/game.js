e63d63217961d924c0f2335935c2add9ed3f23c9
kkasperek <kt.kasperek@gmail.com>
2018-09-24 20:11:28 -0500

    new changes to js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,51 +1,58 @@
 // global variables
 var randomNumber;
-var totalScore = 0; 
+var totalScore = 0;
 var wins = 0;
 var losses = 0;
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-// random number generator ... #19-120 and show 
-function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
-console.log(getRandomNumber(19,120));
-randomNumber = $("#randomNumber").html(getRandomNumber(19,120));
+    // random number generator ... #19-120 and show 
+    function getRandomNumber() {
+        var min = 19;
+        var max = 120;
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    randomNumber = $("#randomNumber").html(getRandomNumber());
+    console.log("random number: " + getRandomNumber());
 
-// function to assign random points to fruits ... 1-12
-var fruitPoints = Math.round(Math.random() * (12 - 0 + 1));
-console.log(fruitPoints);
+    // function to assign random points to fruits ... 1-12
+    function getfruitPoints(fruitPoints) {
+        var min = 1;
+        var max = 12;
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    console.log("fruit points: " + getfruitPoints());
 
-// loop through fruit points array to assign points
+    // created object fruit row where key names = fruit values 
+    var fruitRow = {
+        fruit0: $("#fruit0"),
+        fruit1: $("#fruit1"),
+        fruit2: $("#fruit2"),
+        fruit3: $("#fruit3")
+    };
+    console.log(fruitRow);
 
-var fruit0 = $("#fruit0");
-var fruit1 = $("#fruit1");
-var fruit2 = $("#fruit2");
-var fruit3 = $("#fruit3");
-var fruitImage = [
-    fruit0, fruit1, fruit2, fruit3
-];
+    // loop through object and set each fruit to a random generated value
 
-function getFruitPoints(points) {
-    //1. select id
-    points = $(".fruits"); 
-   //2.
-   //3.
- for(i=0; i < fruitImage; i++){
-    console.log(points);
- }
-
-   // On click of fruit image, take number generated and add it to the total score 
-  $(".fruits").on("click", function() {
-
-    totalScore = totalScore + fruitPoints;
     
 
-  });
+    // On click of fruit image, take number generated and add it to the total score 
+    $(".fruits").on("click", function () {
+        totalScore++;
+        $("#totalScore").html(totalScore);
 
+        //when total score equals random number... else if total score is greater than 
+        if (totalScore === randomNumber) {
+            wins++;
+            $("#wins").html(wins);
+            alert("you win!");
+        } else if (totalScore > randomNumber) {
+            losses++;
+            $("#losses").html(losses);
+        }
 
-  }
+    });
+
 
 
 
