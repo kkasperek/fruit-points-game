c2c84b6acdd7a690bd0118c2f763a031e9361b18
kkasperek <kt.kasperek@gmail.com>
2018-09-24 22:26:57 -0500

    new changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,14 +7,19 @@ var losses = 0;
 $(document).ready(function () {
 
     // random number generator ... #19-120 and show 
-    function getRandomNumber() {
-        var min = 19;
-        var max = 120;
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-    randomNumber = $("#randomNumber").html(getRandomNumber());
-    console.log("random number: " + getRandomNumber());
+    // function getRandomNumber() {
+    //     var min = 19;
+    //     var max = 120;
+    //     return Math.floor(Math.random() * (max - min + 1)) + min;
+    // }
+    // var randomNumber = $("#randomNumber").html(getRandomNumber());
+    // console.log(randomNumber);
 
+    // Shortened random number generator 
+    randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+    $("#randomNumber").html(randomNumber);
+    console.log(randomNumber);
+    
     // function to assign random points to fruits ... 1-12
     function randomPoints() {
         var min = 1;
@@ -32,7 +37,6 @@ $(document).ready(function () {
         let fruitArray = [
             fruit0, fruit1, fruit2, fruit3
         ];
-        console.log(fruitArray);
 
         // loop through fruit array and set each fruit to a random generated value
         for (i = 0; i < fruitArray.length; i++) {
@@ -42,40 +46,43 @@ $(document).ready(function () {
         console.log(fruitArray[0]); //logs random # for first button
 
         // On click of fruit image, take number generated and add it to the total score 
-
+        // work on this it looks terrible but at least its working 
+       
+       
         $('#fruit0').on("click", function () {
             totalScore = totalScore + fruitArray[0];
             $("#totalScore").html(totalScore);
+            console.log(totalScore);
 
         });
         $('#fruit1').on("click", function () {
             totalScore = totalScore + fruitArray[1];
             $("#totalScore").html(totalScore);
-
+            console.log(totalScore);
         });
         $('#fruit2').on("click", function () {
             totalScore = totalScore + fruitArray[2];
             $("#totalScore").html(totalScore);
-
+            return totalScore;
         });
         $('#fruit3').on("click", function () {
             totalScore = totalScore + fruitArray[3];
             $("#totalScore").html(totalScore);
-
+            return totalScore;
         });
-
-        //totalScore = totalScore + getfruitPoints();
-        //$("#totalScore").html(totalScore);
+         
+         console.log(randomNumber);
+    
 
         //when total score equals random number... else if total score is greater than 
-        // if (totalScore === randomNumber) {
-        //     wins++;
-        //     $("#wins").html(wins);
-        //     alert("you win!");
-        // } else if (totalScore > randomNumber) {
-        //     losses++;
-        //     $("#losses").html(losses);
-        // }
+        if (totalScore === randomNumber) {
+            wins++;
+            $("#wins").html(wins);
+            alert("you win!");
+        } else if (totalScore > randomNumber) {
+            losses++;
+            $("#losses").html(losses);
+        }
 
 
     }
